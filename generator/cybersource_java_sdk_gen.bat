d281351747fd11801501e36c0627428ebc0ee96c
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2019-09-05 15:17:49 +0530

    Clean up of batch commands

--- generator/cybersource_java_sdk_gen.bat
+++ generator/cybersource_java_sdk_gen.bat
@@ -6,43 +6,7 @@ rd /s /q ..\docs
 
 java -jar swagger-codegen-cli-2.2.3.jar generate -t cybersource-java-template\libraries\okhttp-gson -i cybersource-rest-spec.json -l java -o ../ -c cybersource-java-config.json
 
-
-powershell -Command " Set-Content ..\src\main\java\Api\CaptureApi.java ((get-content ..\src\main\java\Api\CaptureApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\CreditApi.java ((get-content ..\src\main\java\Api\CreditApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\PaymentsApi.java ((get-content ..\src\main\java\Api\PaymentsApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\PayoutsApi.java ((get-content ..\src\main\java\Api\PayoutsApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\RefundApi.java ((get-content ..\src\main\java\Api\RefundApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\UserManagementApi.java ((get-content ..\src\main\java\Api\UserManagementApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-powershell -Command " Set-Content ..\src\main\java\Api\VoidApi.java ((get-content ..\src\main\java\Api\VoidApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\ReversalApi.java ((get-content ..\src\main\java\Api\ReversalApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\TransactionDetailsApi.java ((get-content ..\src\main\java\Api\TransactionDetailsApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
-
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\SecureFileShareApi.java ((get-content ..\src\main\java\Api\SecureFileShareApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarContentTypes = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarContentTypes = {\"*/*\"};') "
-
-
-
-powershell -Command " Set-Content ..\src\main\java\Api\SearchTransactionsApi.java ((get-content ..\src\main\java\Api\SearchTransactionsApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts ={\"*/*\"};') "
-
-REM powershell -Command " Set-Content ..\src\main\java\Api\PayerAuthenticationApi.java ((get-content ..\src\main\java\Api\PayerAuthenticationApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts = {\"application/hal+json;charset=utf-8\"};') "
+powershell -Command " Set-Content ..\src\main\java\Api\SearchTransactionsApi.java ((get-content ..\src\main\java\Api\SearchTransactionsApi.java -raw) -replace '(?m)(.*)^*final String\[\] localVarAccepts = {[\r\n\s]+\"application\/json;charset=utf-8\"[\r\n\s]+};', 'final String[] localVarAccepts ={\"*/*\"};')"
 
 git checkout ..\pom.xml
 git checkout ..\README.md
