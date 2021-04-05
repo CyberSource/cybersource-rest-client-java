eb143eaa544b58069cefe302a3072ec5bebb4488
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2021-04-05 13:46:50 +0530

    Making logger static in the class

--- src/main/java/utilities/flex/tokenverification/TokenVerificationUtility.java
+++ src/main/java/utilities/flex/tokenverification/TokenVerificationUtility.java
@@ -16,27 +16,23 @@ import utilities.flex.exception.FlexSecurityException;
 import utilities.flex.security.Base64;
 
 public class TokenVerificationUtility {
-	private Logger logger;
-	
-	public TokenVerificationUtility() {
-		this.logger = LogManager.getLogger(this.getClass());
-	}
+	private static Logger logger = LogManager.getLogger(TokenVerificationUtility.class);
 	
 	public boolean verifyToken(PublicKey publicKey, Map<?, ?> postParams) throws FlexException {
 		if (postParams == null) {
-			this.logger.error("FlexSecurityException : A valid Map must be supplied");
+			logger.error("FlexSecurityException : A valid Map must be supplied");
 			throw new FlexSecurityException("A valid Map must be supplied");
 		}
 		final String signature = (String) postParams.get("signature");
 		
 		if (signature == null) {
-			this.logger.error("FlexSecurityException : Missing required field 'signature'");
+			logger.error("FlexSecurityException : Missing required field 'signature'");
 			throw new FlexSecurityException("Missing required field: signature");
 		}
 		final String signedFields = (String) postParams.get("signedFields");
 		
 		if (signedFields == null) {
-			this.logger.error("FlexSecurityException : Missing required field: 'signedFields'");
+			logger.error("FlexSecurityException : Missing required field: 'signedFields'");
 			throw new FlexSecurityException("Missing required field: signedFields");
 		}
 		
@@ -53,17 +49,17 @@ public class TokenVerificationUtility {
 		boolean success = false;
 		
 		if (publicKey == null) {
-			this.logger.error("FlexSecurityException : Must supply a valid PublicKey instance");
+			logger.error("FlexSecurityException : Must supply a valid PublicKey instance");
 			throw new FlexSecurityException("Must supply a valid PublicKey instance");
 		}
 		
 		if (signature == null) {
-			this.logger.error("FlexSecurityException : Missing required field 'signature'");
+			logger.error("FlexSecurityException : Missing required field 'signature'");
 			throw new FlexSecurityException("Missing required field: signature");
 		}
 		
 		if (signedFields == null) {
-			this.logger.error("FlexSecurityException : Missing required field: 'signedFields'");
+			logger.error("FlexSecurityException : Missing required field: 'signedFields'");
 			throw new FlexSecurityException("Missing required field: signedFields");
 		}
 		
@@ -74,10 +70,10 @@ public class TokenVerificationUtility {
 			success = signInstance.verify(Base64.decode(signature));
 			System.out.println(success);
 		} catch (IOException e) {
-			this.logger.error("FlexEncodingException : Unable to decode signature\n{}", e);
+			logger.error("FlexEncodingException : Unable to decode signature\n{}", e);
 			throw new FlexEncodingException("Unable to decode signature"+ e);
 		} catch (GeneralSecurityException e) {
-			this.logger.error("FlexSDKInternalException : {}", e);
+			logger.error("FlexSDKInternalException : {}", e);
 			throw new FlexSDKInternalException(e);
 		}
 		return success;
