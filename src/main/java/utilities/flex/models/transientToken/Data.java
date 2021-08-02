6c85eb9de15d00c279ff1c1ce41940a7c74f5e2c
karthikeyan kumar <karthkum@visa.com>
2021-08-02 12:22:19 +0530

    transientToken/Ctx.java

---
+++ src/main/java/utilities/flex/models/transientToken/Data.java
@@ -0,0 +1,35 @@
+
+package utilities.flex.models.transientToken;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Data {
+
+    @SerializedName("targetOrigins")
+    @Expose
+    private List<String> targetOrigins = null;
+    @SerializedName("mfOrigin")
+    @Expose
+    private String mfOrigin;
+
+    public List<String> getTargetOrigins() {
+        return targetOrigins;
+    }
+
+    public void setTargetOrigins(List<String> targetOrigins) {
+        this.targetOrigins = targetOrigins;
+    }
+
+    public String getMfOrigin() {
+        return mfOrigin;
+    }
+
+    public void setMfOrigin(String mfOrigin) {
+        this.mfOrigin = mfOrigin;
+    }
+
+}
