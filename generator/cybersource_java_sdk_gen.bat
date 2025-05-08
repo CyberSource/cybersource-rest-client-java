ccd8c4f85835426b2c31a62a7c407df11bce09bf
gaubansa <gaubansa@visa.com>
2025-05-08 16:46:48 +0530

    adding changes to handle links field through automated process

--- generator/cybersource_java_sdk_gen.bat
+++ generator/cybersource_java_sdk_gen.bat
@@ -7,6 +7,11 @@ rd /s /q ..\src\test
 rd /s /q ..\target
 rd /s /q ..\docs
 
+setlocal enabledelayedexpansion
+python replaceFieldNamesForPaths.py -i cybersource-rest-spec.json > replaceFieldLogs.log
+del replaceFieldLogs.log
+endlocal
+
 java -jar swagger-codegen-cli-2.4.38.jar generate -t cybersource-java-template\libraries\okhttp-gson -i cybersource-rest-spec-java.json -l java -o ../ -c cybersource-java-config.json
 
 REM Changing in Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifierProcessingInformationAuthorizationOptionsInitiatorMerchantInitiatedTransaction.java
@@ -28,6 +33,11 @@ powershell Rename-Item ..\src\main\java\Model\Tmsv2customersEmbeddedDefaultPayme
 powershell Rename-Item ..\docs\Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifierProcessingInformationAuthorizationOptionsInitiatorMerchantInitiatedTransaction.md Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedMerchantInitiatedTransaction.md
 
 powershell -Command " Set-Content ..\src\main\java\Api\SecureFileShareApi.java ((get-content ..\src\main\java\Api\SecureFileShareApi.java -raw) -replace '\*_\/_\*;charset=utf-8', '*/*;charset=utf-8') "
+
+@REM replace sdkLinks fieldName to links for supporting links field name in request/response body
+echo "starting of replacing the links keyword in PblPaymentLinksAllGet200Response.java model"
+powershell -Command " Set-Content ..\src\main\java\Model\PblPaymentLinksAllGet200Response.java ((get-content ..\src\main\java\Model\PblPaymentLinksAllGet200Response.java -raw) -replace '@SerializedName\(\"sdkLinks\"\)', '@SerializedName(\"links\")')"
+echo "completed the task of replacing the links keyword in PblPaymentLinksAllGet200Response.java model"
 
 git checkout ..\src\main\java\Api\OAuthApi.java
 git checkout ..\src\main\java\Model\AccessTokenResponse.java
